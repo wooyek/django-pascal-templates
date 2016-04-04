7bf8eadbed1c825c43543b44bddc6ba97833161f
Janusz Skonieczny <git@wooyek.com>
2016-04-04 22:54:09 +0200

    Bump version: 0.8.3 → 0.8.4

--- src/pascal_templates/__init__.py
+++ src/pascal_templates/__init__.py
@@ -6,5 +6,5 @@
 # PROPRIETY TRADE SECRETS of Brave Labs sp. z o.o.
 # Use is subject to license terms. See NOTICE file of this project for details.
 
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 from .views import *

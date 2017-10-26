322639062a72c5c76fa4a1071176ed581e17ba26
Janusz Skonieczny <git@wooyek.com>
2017-10-26 15:30:14 +0200

    Bump version: 0.8.10 → 0.8.11

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.8.10"
+version = "0.8.11"
 
 setup_kwargs = {
     'name': "django-pascal-templates",

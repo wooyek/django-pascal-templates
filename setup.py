4c2216de7f7e53c7db6c2ab1f75f0b9065ce580e
Janusz Skonieczny <git.js@bravelabs.pl>
2018-04-17 19:43:57 +0200

    Bump version: 0.8.12 → 0.8.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = parse_requirements(
 with io.open("README.rst", encoding="UTF-8") as readme:
     long_description = readme.read()
 
-version = "0.8.12"
+version = "0.8.13"
 
 setup_kwargs = {
     'name': "django-pascal-templates",
--- src/pascal_templates/__init__.py
+++ src/pascal_templates/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.8.12"
+__version__ = "0.8.13"
 from .views import *
 
